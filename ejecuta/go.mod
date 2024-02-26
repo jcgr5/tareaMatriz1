894dd038b84b05ce17572a118637f81b9ddcb2e4
juan <jcgr1709@gmail.com>
2024-02-26 15:24:47 -0500

    por fin :)

--- ejecuta/go.mod
+++ ejecuta/go.mod
@@ -1,3 +1,5 @@
 module github.com/jcgr5/tareaMatriz1
 
 go 1.21.6
+
+require github.com/jcgr5/tareaMatriz1/operaciones v0.0.0-20240226043119-2e872ed5f0a6
--- operaciones/generar/generar.go
+++ operaciones/generar/generar.go
@@ -2,9 +2,11 @@ package generar
 
 import "math/rand"
 
-func CrearMatriz() [2]int {
-	m := rand.Intn(3)
-	n := rand.Intn(3)
-	vector := [2]int{m, n}
+func CrearMatriz() [2][2]int {
+	m, n := rand.Intn(8)+1, rand.Intn(8)+1
+	vector := [2][2]int{
+		{m, n},
+		{n, m},
+	}
 	return vector
 }
