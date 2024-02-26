5ecf1afe16d3e55348efbcb26af075a7229d6139
juan <jcgr1709@gmail.com>
2024-02-25 23:01:00 -0500

    quinto commit, agregué un directorio en operaciones

--- ejecuta/main.go
+++ ejecuta/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	generar "github.com/jcgr5/tareaMatriz1/blob/main/operaciones/generar.go"
+	"github.com/jcgr5/tareaMatriz1/blob/main/operaciones/generar"
 )
 
 func main() {
